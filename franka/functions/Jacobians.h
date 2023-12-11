84e12460edc45deb8339cd5e40c218ec5964fa85
rac182 <r.nebbiacolomba@studenti.unipi.it>
2023-12-11 17:16:27 +0100

    clean

--- franka/functions/Jacobians.h
+++
@@ -1,8 +0,0 @@
-#ifndef JACOBIANS_H
-#define JACOBIANS_H
-
-void get_Ja_proj(const double q[7], double Ja[42]);
-
-void get_Ja_dot_proj(const double q[7], const double dq[7], double Ja_dot[42]);
-
-#endif
